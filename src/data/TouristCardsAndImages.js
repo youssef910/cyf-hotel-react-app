0de51c2353f56b3cf5338cb420b50074f5f8f32a
youssef sadek <47015754+youssef910@users.noreply.github.com>
2021-10-19 19:33:32 +0100

    crate tourist cards data

---
+++ src/data/TouristCardsAndImages.js
@@ -0,0 +1,28 @@
+import glasgowImage from "./glasgowImage.jpg";
+import londonImage from "./londonImage.jpg";
+import manchesterImage from "./manchesterImage.jpg";
+const TouristCardsAndImages = [
+  {
+    cityName: "Glasgow",
+    link: "https://peoplemakeglasgow.com/",
+    photo: glasgowImage,
+    description:
+      "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."
+  },
+  {
+    cityName: "Manchester",
+    link: "https://www.visitmanchester.com/",
+    photo: manchesterImage,
+    description:
+      "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."
+  },
+  {
+    cityName: "London",
+    link: "https://www.visitlondon.com/",
+    photo: londonImage,
+    description:
+      "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."
+  }
+];
+
+export default TouristCardsAndImages;
