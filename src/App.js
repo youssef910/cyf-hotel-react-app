86b8f4f656582691053fe1e833a4cf76a89e4289
youssef sadek <47015754+youssef910@users.noreply.github.com>
2021-10-19 19:32:20 +0100

    Rendering The components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
-
 import Bookings from "./Bookings";
 import "./App.css";
-
+import Header from "./Header";
+import TouristInfoCards from "./TouristInfoCards";
+import Footer from "./Footer";
+import footerDetails from "./data/FooterDetails";
+import Restaurant from "./Restaurant";
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">CYF Hotel</header>
+    <div className="container-fluid">
+      <Header />
       <Bookings />
+      <Restaurant />
+      <TouristInfoCards />
+      <Footer footerDetails={footerDetails} />
     </div>
   );
 };
