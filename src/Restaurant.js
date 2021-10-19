173e261f9e777cde7f8a45a56f14d588225cbb37
youssef sadek <47015754+youssef910@users.noreply.github.com>
2021-10-19 19:36:30 +0100

    adding  to order in restuarant

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import RestaurantButton from "./RestaurantButton";
 
 const Restaurant = () => {
-  const pizzas = 0;
+  const initialState = 0;
+  const [order, setOrder] = useState(initialState);
+  const orderOne = () => {
+    setOrder(currentOrder => currentOrder + 1);
+  };
+
   return (
     <div>
       <h3>Restaurant Orders</h3>
       <ul>
         <li>
-          Pizzas: {pizzas} <button className="btn btn-primary">Add</button>
+          Pizzas: {order}{" "}
+          <RestaurantButton name="Add" clickToAddToOrder={orderOne} />
         </li>
       </ul>
     </div>
